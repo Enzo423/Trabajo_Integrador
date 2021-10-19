2d7d85b9def41f26cb10547dfae874d4ad7a60be
Enzo <you@example.com>
2021-10-19 20:36:49 -0300

    Se agrega el archivo testEmpleado.php

--- tests/testEmpleado.php
+++ tests/testEmpleado.php
@@ -1,5 +1,18 @@
 <?php 
-
-class EmpleadoTest extends \PHPUnit\framework\TestCase
+class EmpleadoTest extends \PHPUnit\Framework\TestCase
 {
-	public function Test
+	public function testSePuedenObtenerLosDatos()
+	{
+		$r = new \App\Empleado("Enzo", "Sandoval", 44021952, 25000);
+		$this->assertEquals( 44021952, $r->getDNI());
+	}
+	
+	 public function testNoSePuedeCrearRectanguloConMedidasNegativas()
+    {
+        $this->expectException(\Exception::class);
+        //Lanzará una excepción, porque la base no puede ser cero:
+        $r = new \App\Empleado("", "", 0, 25000, "eCommerce");
+    }	
+	
+}
+?>
