4092735e336f324fb19f92453506063ce38685b7
Enzo <you@example.com>
2021-10-20 18:19:47 -0300

    Se modifica el archivo EmpleadoEventualTest.php agregando el test 'testSePuedeCalcularLaComision()' para probar que el método calcularComision() funciona como se espera.

--- tests/EmpleadoEventualTest.php
+++ tests/EmpleadoEventualTest.php
@@ -1,7 +1,13 @@
 <?php 
-class EmpleadoTest extends \PHPUnit\Framework\TestCase
+class EmpleadoEventualTest extends \PHPUnit\Framework\TestCase
 {
-
+    public function testSePuedeCalcularLaComision()
+    {
+			$r = new \App\EmpleadoEventual($nombre = "Enzo", $apellido = "Sandoval", $dni = 44021952, $salario = 25000, $montosDeVentas = [3000, 4000]);
+			$this->assertEquals( (3000+4000) / 2 *0.05, $r->calcularComision());
+	}
+	
+	
 	
 }
 ?>
