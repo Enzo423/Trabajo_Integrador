83b8fc90a139651d67e4ffb1ed83bc8cdca96bef
Enzo <you@example.com>
2021-10-20 20:25:30 -0300

    Se modifica el archivo EmpleadoPermanenteTest.php agregando el test 'testSePuedeCalcularAntiguedad()' para probar que el método calcularAntiguedad() funciona como se espera para un empleado con varios años de antigüedad.

--- tests/EmpleadoPermanenteTest.php
+++ tests/EmpleadoPermanenteTest.php
@@ -7,5 +7,20 @@ class EmpleadoPermamenteTest extends \PHPUnit\Framework\TestCase
 			$this->assertEquals( $r->calcularAntiguedad() . "%", $r->calcularComision());
 	}
 	
+	public function testSePuedeCalcularElIngresoTotal()
+    {
+			$r = new \App\EmpleadoPermanente($nombre = "Enzo", $apellido = "Sandoval", $dni = 44021952, $salario = 25000, $fechaIngreso = new \DateTime('22-04-2018'));
+			$this->assertEquals( ($salario + $salario * $r->calcularAntiguedad() / 100), $r->calcularIngresoTotal());
+	}
+	
+	public function testSePuedeCalcularAntiguedad()
+    {
+			$r = new \App\EmpleadoPermanente($nombre = "Enzo", $apellido = "Sandoval", $dni = 44021952, $salario = 25000, $fechaIngreso = new \DateTime('2001-04-21'));
+            $fechaActual =  new \DateTime();
+            $diff = $fechaIngreso->diff($fechaActual);
+            $dias = $diff->format("%y");
+			$this->assertEquals( $dias, $r->calcularAntiguedad());
+	}
+	
 }
 ?>
