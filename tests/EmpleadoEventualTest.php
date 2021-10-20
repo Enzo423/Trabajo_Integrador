ff24fd2e1e612778fd23930ccec7c4994f048a1c
Enzo <you@example.com>
2021-10-20 18:28:38 -0300

    Se modifica el archivo EmpleadoEventualTest.php agregando el test 'testNoSePuedeCrearMontoNegativoCero()' para probar que si intento construir un empleado proporcionando algún monto de venta negativo o cero, lanza una excepción.

--- tests/EmpleadoEventualTest.php
+++ tests/EmpleadoEventualTest.php
@@ -13,5 +13,12 @@ class EmpleadoEventualTest extends \PHPUnit\Framework\TestCase
 			$this->assertEquals( ($r->calcularComision() + $salario), $r->calcularIngresoTotal());
 	}
 	
+	public function testNoSePuedeCrearMontoNegativoCero()
+    {
+        $this->expectException(\Exception::class);
+        $r = new \App\EmpleadoEventual($nombre = "Enzo", $apellido = "Sandoval", $dni = 44021952, $salario = 25000, $montosDeVentas = [3000, 0]);
+		
+    }
+	
 }
 ?>
