35364da62cb4e2ab5669ae0ae6a0c1b31ac006e7
Enzo <you@example.com>
2021-10-20 17:37:22 -0300

    Se agrega el test 'testNoSePuedeCrearNombreEmpty' en EmpleadoTest.php para probar que si intento construir un empleado con el nombre vacío, lanza una excepción.

--- tests/EmpleadoTest.php
+++ tests/EmpleadoTest.php
@@ -3,16 +3,22 @@ class EmpleadoTest extends \PHPUnit\Framework\TestCase
 {
 	public function testSePuedenObtenerLosDatos()
 	{
-		$r = new \App\Empleado($nombre = "Enzo", $apellido = "Sandoval", $dni = 44021952, $salario = 25000, $sector = "");
+		$r = new \App\Empleado($nombre = "Enzo", $apellido = "Sandoval", $dni = 44021952, $salario = 25000, $sector = "X");
         $this->assertEquals( $nombre . " " . $apellido, $r->getNombreApellido());
 		$this->assertEquals( $dni, $r->getDNI());
 		$this->assertEquals( $salario, $r->getSalario());
+		//setSector($sector);
 		$this->assertEquals( $sector, $r->getSector());
 		$this->assertEquals( $nombre . " " . $apellido . " " . $dni . " " . $salario, $r->__toString());
 	}
 	
-	
-	
+	 public function testNoSePuedeCrearNombreEmpty()
+    {
+		
+        $this->expectException(\Exception::class);
+        $r = new \App\Empleado($nombre = "", $apellido = "Sandoval", $dni = 44021952, $salario = 25000, $sector="No especificado");
+		
+    }
 	
 }
 ?>
