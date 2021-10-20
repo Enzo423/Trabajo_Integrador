fc6ae3236233f54a4eb999748b4a017b3eaf0bb8
Enzo <you@example.com>
2021-10-20 18:24:29 -0300

    Se modifica el archivo EmpleadoEventualTest.php agregando el test 'testSePuedeCalcularElIngresoTotal()' para probar que el método calcularIngresoTotal() funciona como se espera.

--- tests/EmpleadoEventualTest.php
+++ tests/EmpleadoEventualTest.php
@@ -7,7 +7,11 @@ class EmpleadoEventualTest extends \PHPUnit\Framework\TestCase
 			$this->assertEquals( (3000+4000) / 2 *0.05, $r->calcularComision());
 	}
 	
-	
+	public function testSePuedeCalcularElIngresoTotal()
+    {
+			$r = new \App\EmpleadoEventual($nombre = "Enzo", $apellido = "Sandoval", $dni = 44021952, $salario = 25000, $montosDeVentas = [3000, 4000]);
+			$this->assertEquals( ($r->calcularComision() + $salario), $r->calcularIngresoTotal());
+	}
 	
 }
 ?>
