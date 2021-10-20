13ad069ea121d41bb7e0e55686faeb0d8468172e
Enzo <you@example.com>
2021-10-20 19:56:29 -0300

    Se modifica el archivo EmpleadoPermanenteTest.php agregando el test 'testSePuedeCalcularLaComision()' para probar que el método calcularComision() funciona como se espera.

--- tests/EmpleadoPermanenteTest.php
+++ tests/EmpleadoPermanenteTest.php
@@ -1,7 +1,11 @@
 <?php 
 class EmpleadoPermamenteTest extends \PHPUnit\Framework\TestCase
 {
-    
+    public function testSePuedeCalcularLaComision()
+    {
+			$r = new \App\EmpleadoPermanente($nombre = "Enzo", $apellido = "Sandoval", $dni = 44021952, $salario = 25000, $fechaIngreso = new \DateTime('22-04-2018'));
+			$this->assertEquals( $r->calcularAntiguedad() . "%", $r->calcularComision());
+	}
 	
 }
 ?>
