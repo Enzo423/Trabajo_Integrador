163993851d9496ed7e9ef5f0ea04f2a0b2145a99
Enzo <you@example.com>
2021-10-20 21:08:28 -0300

    Se modifica el archivo EmpleadoPermanenteTest.php agregandole al test 'testSePuedeObtenerFecha()' fecha constante a .

--- tests/EmpleadoPermanenteTest.php
+++ tests/EmpleadoPermanenteTest.php
@@ -3,7 +3,7 @@ class EmpleadoPermamenteTest extends \PHPUnit\Framework\TestCase
 {
 	public function testSePuedeObtenerLaFecha()
     {
-			$r = new \App\EmpleadoPermanente($nombre = "Enzo", $apellido = "Sandoval", $dni = 44021952, $salario = 25000, $fechaIngreso = new \DateTime());
+			$r = new \App\EmpleadoPermanente($nombre = "Enzo", $apellido = "Sandoval", $dni = 44021952, $salario = 25000, $fechaIngreso = new \DateTime('22-04-2018'));
 			$this->assertEquals( $fechaIngreso, $r->getFechaIngreso());
 	}
 	
